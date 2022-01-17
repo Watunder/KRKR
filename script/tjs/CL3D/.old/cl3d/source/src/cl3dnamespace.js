9fd98194ecb0f46935490097a4b2f1836b315ac0
Watunder <77280887+Watunder@users.noreply.github.com>
2022-01-18 03:08:50 +0800

    ignore script/tjs/CL3D/.old

--- script/tjs/CL3D/.old/cl3d/source/src/cl3dnamespace.js
+++
@@ -1,8 +0,0 @@
-//+ Nikolaus Gebhardt
-// This file is part of the CopperLicht library, copyright by Nikolaus Gebhardt
-
-/**
- * The CopperLicht namespace containing all classes and some helper functions
- * @namespace The main namespace for CopperLicht
-  */
-var CL3D = {}
--- script/tjs/CL3D/.old/cl3d/source/src/headercomment_closure.js
+++
@@ -1,4 +0,0 @@
-/**
-  * CopperCube player implemented using the CopperLicht 3D Engine, (c) by Nikolaus Gebhardt, Ambiera e.U.
-  * http://www.ambiera.com/coppercube
-  */
